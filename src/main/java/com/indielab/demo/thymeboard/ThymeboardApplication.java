7b76a0df626111afa25591af78608862f624eeab
John Song <darkfish0817@gmail.com>
2024-07-31 11:23:57 +0900

    initial commit

---
+++ src/main/java/com/indielab/demo/thymeboard/ThymeboardApplication.java
@@ -0,0 +1,14 @@
+package com.indielab.demo.thymeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeboardApplication.class, args);
+    }
+
+    // need to text to commit
+}
---
+++ src/test/java/com/indielab/demo/thymeboard/ThymeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indielab.demo.thymeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
